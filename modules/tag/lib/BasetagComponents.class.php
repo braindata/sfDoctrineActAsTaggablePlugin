e6d47699171b519880e1fcd7ed3b18526e21bc8d
Andre Salmon <andre.salmon@braindata.de>
2014-11-17 15:22:17 +0100

    FIX: Fix config load

--- modules/tag/lib/BasetagComponents.class.php
+++ modules/tag/lib/BasetagComponents.class.php
@@ -13,7 +13,7 @@ abstract class BasetagComponents extends sfComponents
   public function executePopularTags() {
     $q = Doctrine_Query::create()
       ->where('t.is_featured = 0')
-      ->limit(sfConfig::get('max_popular_tags_in_header', 10));
+      ->limit(sfConfig::get('app_max_popular_tags_in_header', 10));
     $this->tags = PluginTagTable::getPopulars($q);
     arsort($this->tags);
 
